d24e5732418dc79e3a28dc6cffc5af89e742aebd
donniedata <donnell.jackson@outlook.com>
2024-01-01 01:02:53 -0600

    creating function for loading new dataset

--- queries.sql
+++ queries.sql
@@ -23,13 +23,64 @@ create OR REPLACE TABLE op_database.public.nyc_payroll_data (
 	"total_other_pay" VARCHAR(50)
 );
 
+
+
+CREATE OR REPLACE TABLE op_database.public.nyc_parking_violations_fy_23 ( 
+"summons_number" VARCHAR(50),
+ "plate_id" VARCHAR(50),
+ "registration_state" VARCHAR(50),
+ "plate_type" VARCHAR(50),
+ "issue_date" VARCHAR(50),
+ "violation_code" VARCHAR(50),
+ "vehicle_body_type" VARCHAR(50),
+ "vehicle_make" VARCHAR(50),
+ "issuing_agency" VARCHAR(50),
+ "street_code1" VARCHAR(50),
+ "street_code2" VARCHAR(50),
+ "street_code3" VARCHAR(50),
+ "vehicle_expiration_date" VARCHAR(50),
+ "violation_location" VARCHAR(50),
+ "violation_precinct" VARCHAR(50),
+ "issuer_precinct" VARCHAR(50),
+ "issuer_code" VARCHAR(50),
+ "issuer_command" VARCHAR(50),
+ "issuer_squad" VARCHAR(50),
+ "violation_time" VARCHAR(50),
+ "time_first_observed" VARCHAR(50),
+ "violation_county" VARCHAR(50),
+ "violation_in_front_of_or" VARCHAR(50),
+ "house_number" VARCHAR(50),
+ "street_name" VARCHAR(50),
+ "intersecting_street" VARCHAR(50),
+ "date_first_observed" VARCHAR(50),
+ "law_section" VARCHAR(50),
+ "sub_division" VARCHAR(50),
+ "violation_legal_code" VARCHAR(50),
+ "days_parking_in_effect" VARCHAR(50),
+ "from_hours_in_effect" VARCHAR(50),
+ "to_hours_in_effect" VARCHAR(50),
+ "vehicle_color" VARCHAR(50),
+ "unregistered_vehicle" VARCHAR(50),
+ "vehicle_year" VARCHAR(50),
+ "meter_number" VARCHAR(50),
+ "feet_from_curb" VARCHAR(50),
+ "violation_post_code" VARCHAR(50),
+ "violation_description" VARCHAR(50),
+ "no_standing_or_stopping" VARCHAR(50),
+ "hydrant_violation" VARCHAR(50),
+ "double_parking_violation" VARCHAR(50)
+
+)
+
+
+
 --load data into table 
 COPY INTO nyc_payroll_data 
 from @%nyc_payroll_data
 FILE_FORMAT = ( SKIP_HEADER = 1 )
 on_error = 'continue';
 
-
+a
 --results from laod into job 
 select * 
 from table(RESULT_SCAN('01b141d3-0001-99d2-0002-73020002b09a'));
